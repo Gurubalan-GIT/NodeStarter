051183fce6fe5c3548f4701bfa53a292a9538288
Gurubalan-GIT <gurubalan.work@gmail.com>
2019-03-31 19:23:16 +0530

    Pushed express generator fix\

---
+++ Example-8/routes/dishRouter.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const dishRouter = express.Router();
+
+dishRouter.use(bodyParser.json());
+
+//Now here I'm routing if one /dishes/ comes in as an end point
+dishRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the dishes to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all dishes');
+});
+
+//And here I'm handling with ID's as a request with a dedicated end point for it 
+dishRouter.route('/:dishId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
+})
+.post((req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+})
+.put((req, res, next) => {
+  res.write('Updating the dish: ' + req.params.dishId + '\n');
+  res.end('Will update the dish: ' + req.body.name + 
+        ' with details: ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+
+//Exporting it so that we can require/import it in the index.js file
+module.exports = dishRouter;
