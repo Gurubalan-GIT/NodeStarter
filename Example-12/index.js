c8dc5ed11a896adce5f09a363f4aa30fb015592a
Gurubalan-GIT <gurubalan.work@gmail.com>
2019-04-03 00:30:50 +0530

    Pushed mongoose promises

--- Example-12/index.js
+++ Example-12/index.js
@@ -10,11 +10,13 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 //Created a document according to the Schema
-    var newDish = Dishes({
+//Method -1 :
+
+   /* var newDish = Dishes({
         name: 'Gurubalan',
         description: 'test'
     });
-//Saving it 
+    //Saving it 
     newDish.save()
         .then((dish) => {
             console.log(dish);
@@ -35,4 +37,29 @@ connect.then((db) => {
         });
 }).catch((err)=>{
     console.log(err);
-});
+}); */
+
+//Method -2 : Directly using it with the exported Schema and passing in the document as the parameter 
+        Dishes.create({
+            name: 'Gurubalan-1.0',
+            description: 'Test'
+        })
+        .then((dish) => {
+            console.log(dish);
+            
+            return Dishes.find({}).exec();
+        })
+        .then((dishes) => {
+            console.log(dishes);
+
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }).catch((err)=>{
+        console.log(err);
+})
